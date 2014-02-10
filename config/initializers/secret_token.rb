e9a647590079c855cc662e2d2598515c4eb26ce8
MATC Student <student@MATC1234.(none)>
2014-02-10 16:59:24 -0600

    rails new first_app -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '65fd54246f24fd793111fd190e486fb49a7a975b03b2a13cd587dcd1528377529ad0010e8b22012d05bb51baffb463f06929bd056cfa82ffd27194b48248f86f'
